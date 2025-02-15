5a8a56e20c60c7b5b077535e7f3671fbb5ab529e
maksim.antonovich <maksim.antonovich@lightspeedhq.com>
2025-02-15 20:28:09 +0400

    fucking day 12 part 2

--- src/main/java/org/example/days/Day12.java
+++ src/main/java/org/example/days/Day12.java
@@ -16,19 +16,25 @@ public class Day12 {
     private final Map<Integer, Integer> sides = new HashMap<>();
     private int regionCounter = 1;
 
+    static class Coordinate {
+        public int i, j;
+
+        public Coordinate(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
     private static final int[][] DIRECTIONS = {
             {-1, 0}, {1, 0}, {0, -1}, {0, 1}
     };
-    private static final double[][] DIAGONALS = {
-            {0.5, -0.5}, {0.5, 0.5}, {-0.5, 0.5}, {-0.5, 0.5}
-    };
-
 
     private void part1() {
         matrix = Utils.readFileCharMatrix("input/day12.txt");
         markMatrix();
 
-        calculateAreaAndPerimeter();
+        calculateArea();
+        calculatePerimeter();
 
         Utils.outputMatrix(markedMatrix);
 
@@ -40,12 +46,11 @@ public class Day12 {
         System.out.println("Answer = " + answer);
     }
 
-    // 823953 too low
     private void part2() {
-        matrix = Utils.readFileCharMatrix("input/day12_test.txt");
+        matrix = Utils.readFileCharMatrix("input/day12.txt");
         markMatrix();
-
-        calculateAreaAndSides();
+        calculateArea();
+        calculateSides();
 
         Utils.outputMatrix(markedMatrix);
 
@@ -57,23 +62,89 @@ public class Day12 {
         System.out.println("Answer = " + answer);
     }
 
-    private void calculateAreaAndSides() {
+    private void calculateSides() {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 int regionId = markedMatrix[i][j];
-                if (regionId == 0) continue; // Skip unmarked cells
+                int cornersCount = 0;
 
-                area.merge(regionId, 1, Integer::sum);
-
-                // Check all four edges of the current cell
-                for (int[] direction : DIRECTIONS) {
-                    int ni = i + direction[0];
-                    int nj = j + direction[1];
+                // ---OUTSIDE CORNERS---
+                // top left corner
+                if ((!isValidCell(i - 1, j) || markedMatrix[i - 1][j] != regionId) &&
+                    (!isValidCell(i, j - 1) || markedMatrix[i][j - 1] != regionId)) {
+                    logCorner(i, j, regionId, "top left");
+                    cornersCount++;
                 }
+
+                // top right corner
+                if ((!isValidCell(i - 1, j) || markedMatrix[i - 1][j] != regionId) &&
+                    (!isValidCell(i, j + 1) || markedMatrix[i][j + 1] != regionId)) {
+                    logCorner(i, j, regionId, "top right");
+                    cornersCount++;
+                }
+
+                // bottom left corner
+                if ((!isValidCell(i + 1, j) || markedMatrix[i + 1][j] != regionId) &&
+                    (!isValidCell(i, j - 1) || markedMatrix[i][j - 1] != regionId)) {
+                    logCorner(i, j, regionId, "bottom left");
+                    cornersCount++;
+                }
+
+                // bottom right corner
+                if ((!isValidCell(i + 1, j) || markedMatrix[i + 1][j] != regionId) &&
+                    (!isValidCell(i, j + 1) || markedMatrix[i][j + 1] != regionId)) {
+                    logCorner(i, j, regionId, "bottom right");
+                    cornersCount++;
+                }
+                // ---END OUTSIDE CORNERS---
+
+                // ---INSIDE CORNERS---
+                // top left inside corner
+                if ((i < markedMatrix.length - 1 && j < markedMatrix[0].length - 1) &&
+                        markedMatrix[i + 1][j + 1] != regionId &&
+                        markedMatrix[i + 1][j] == regionId &&
+                        markedMatrix[i][j + 1] == regionId) {
+                    logCorner(i, j, regionId, "top left inside");
+                    cornersCount++;
+                }
+
+                // top right inside corner
+                if ((i < markedMatrix.length - 1 && j > 0) &&
+                        markedMatrix[i + 1][j - 1] != regionId &&
+                        markedMatrix[i + 1][j] == regionId &&
+                        markedMatrix[i][j - 1] == regionId) {
+                    logCorner(i, j, regionId, "top right inside");
+                    cornersCount++;
+                }
+
+                // bottom left inside corner
+                if ((i > 0 && j < markedMatrix[0].length - 1) &&
+                        markedMatrix[i - 1][j + 1] != regionId &&
+                        markedMatrix[i - 1][j] == regionId &&
+                        markedMatrix[i][j + 1] == regionId) {
+                    logCorner(i, j, regionId, "bottom left inside");
+                    cornersCount++;
+                }
+
+                // bottom right inside corner
+                if ((i > 0 && j > 0) &&
+                        markedMatrix[i - 1][j - 1] != regionId &&
+                        markedMatrix[i - 1][j] == regionId &&
+                        markedMatrix[i][j - 1] == regionId) {
+                    logCorner(i, j, regionId, "bottom right inside");
+                    cornersCount++;
+                }
+
+                // ---END INSIDE CORNERS---
+
+                sides.merge(regionId, cornersCount, Integer::sum);
             }
         }
     }
 
+    private void logCorner(int i, int j, int regionId, String condition) {
+        System.out.println("regionId = " + regionId + ", i = " + i + ", j = " + j + ", " + condition);
+    }
 
     private void markMatrix() {
         markedMatrix = new int[matrix.length][matrix[0].length];
@@ -93,7 +164,6 @@ public class Day12 {
         }
     }
 
-
     private void markRegion(int i, int j, int regionId) {
         if (!isValidCell(i, j) || markedMatrix[i][j] != 0) {
             return;
@@ -110,12 +180,19 @@ public class Day12 {
         }
     }
 
-    private void calculateAreaAndPerimeter() {
+    private void calculateArea() {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 int regionId = markedMatrix[i][j];
                 area.merge(regionId, 1, Integer::sum);
+            }
+        }
+    }
 
+    private void calculatePerimeter() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int regionId = markedMatrix[i][j];
                 int localPerimeter = calculateCellPerimeter(i, j);
                 perimeter.merge(regionId, localPerimeter, Integer::sum);
             }
@@ -136,5 +213,4 @@ public class Day12 {
     private boolean isValidCell(int i, int j) {
         return i >= 0 && i < matrix.length && j >= 0 && j < matrix[0].length;
     }
-
 }
