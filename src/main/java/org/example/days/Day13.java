c7f50a081b172d5329d2df60e7b2fbfdde9af650
maksim.antonovich <maksim.antonovich@lightspeedhq.com>
2025-02-17 17:15:04 +0400

    Day 13 init

---
+++ src/main/java/org/example/days/Day13.java
@@ -0,0 +1,16 @@
+package org.example.days;
+
+import org.example.utils.Utils;
+
+import java.util.List;
+
+public class Day13 {
+    public static void main(String[] args) {
+        new Day13().part1();
+    }
+
+    public void part1() {
+        List<String> input = Utils.readFile("input/day13_test.txt");
+        
+    }
+}
